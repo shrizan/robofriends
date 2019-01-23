fff329789214bee6a3e7699cd74dc294335f3af2
shree_acharya <sreejan.acharya@outlook.com>
2019-01-23 14:56:38 +0545

    Updates

---
+++ precache-manifest.acb43b731302af4632e0c21c9c981740.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "1446d6ec194445c3572d",
+    "url": "/robofriends/static/js/runtime~main.1446d6ec.js"
+  },
+  {
+    "revision": "097b20b627bebe3aeb6a",
+    "url": "/robofriends/static/js/main.097b20b6.chunk.js"
+  },
+  {
+    "revision": "56e33d0810ce04d442f1",
+    "url": "/robofriends/static/js/1.56e33d08.chunk.js"
+  },
+  {
+    "revision": "097b20b627bebe3aeb6a",
+    "url": "/robofriends/static/css/main.dfefd453.chunk.css"
+  },
+  {
+    "revision": "56e33d0810ce04d442f1",
+    "url": "/robofriends/static/css/1.9849da83.chunk.css"
+  },
+  {
+    "revision": "f4f51832a3959de8c18d6130567b65aa",
+    "url": "/robofriends/index.html"
+  }
+];
